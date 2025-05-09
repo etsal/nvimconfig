69b620c19093b1515ec7f42435ff4b419a7c7e62
Emil Tsalapatis <etsal@meta.com>
2025-05-09 08:24:24 -0700

    properly pull in plugins on setup

--- setup.sh
+++ setup.sh
@@ -2,12 +2,19 @@
 
 EXTENSIONS="tokyonight.nvim nvim-lspconfig fzf-lua"
 
+rm -rf ~/.config/nvim
+mkdir -p ~/.config/nvim
+
 cp bsdsty.lua ~/.config/nvim/init.lua
 
 rm -rf ~/.local/share/nvim/site/pack/plugins/start/
 mkdir -p ~/.local/share/nvim/site/pack/plugins/start/
 
 for extension in $EXTENSIONS; do
+	cd $extension;
+	git submodule init;
+	git submodule update;
+	cd -
 	cp -r $extension ~/.local/share/nvim/site/pack/plugins/start/
 done
 
