33ff1a12a765b0d370d8816a6ff2f1dd03afbdac
Emil Tsalapatis <emil@etsalapatis.com>
2025-06-06 00:44:32 -0400

    add xterm config and themes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 EXTENSIONS="tokyonight.nvim nvim-lspconfig fzf-lua"
+OTHER="Xresources-themes"
 
 rm -rf ~/.config/nvim
 mkdir -p ~/.config/nvim
@@ -10,11 +11,14 @@ cp bsdsty.lua ~/.config/nvim/init.lua
 rm -rf ~/.local/share/nvim/site/pack/plugins/start/
 mkdir -p ~/.local/share/nvim/site/pack/plugins/start/
 
-for extension in $EXTENSIONS; do
-	cd $extension;
+for repo in $EXTENSIONS $OTHER; do
+	cd $repo;
 	git submodule init;
 	git submodule update;
 	cd -
+done
+
+for extension in "$EXTENSIONS"; do
 	cp -r $extension ~/.local/share/nvim/site/pack/plugins/start/
 done
 
@@ -27,3 +31,10 @@ sudo dnf install -y i3 i3lock i3status nautilus
 mkdir -p $HOME/.config/i3
 cp $PWD/i3_config $HOME/.config/i3
 
+# Install fonts for xterm
+mkdir -p $HOME/.local/share
+cp -r Xresources-themes $HOME/.local/share/Xresources-themes
+
+# Install xterm itself
+sudo dnf install -y xterm
+cp $PWD/Xresources ~/.Xresources
